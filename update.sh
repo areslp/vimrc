7a636f9a7ad907a1c0c2e6998c53e793f5930c2a
luopei <luopei@holomatic.com>
2018-07-09 11:19:59 +0800

    update script

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ set -e
 base=`pwd`
 echo $base
 
-install_projects=(holo_base holo_3d holo_map)
+install_projects=(holo_base holo_3d holo_sensors holo_map holo_data_provider holo_localization holo_calibration)
 
 for project in ${install_projects[@]}
 do
@@ -11,11 +11,12 @@ do
     set +e
     cd $project && git pull && mkdir build 
     set -e
-    cd build && cmake .. && make && make test && sudo make install
+    # cd build && cmake .. && make && make test && sudo make install
+    cd build && cmake .. && make && sudo make install
     cd $base
 done
 
-projects=(holo_calibration holo_data_provider holo_planning holo_sensors holo_cmw holo_vis holo_simulator holo_localization)
+projects=(holo_planning holo_cmw holo_vis holo_simulator)
 
 for project in ${projects[@]}
 do
@@ -23,11 +24,12 @@ do
     set +e
     cd $project && git pull && mkdir build 
     set -e
-    cd build && cmake .. && make && make test
+    # cd build && cmake .. && make && make test
+    cd build && cmake .. && make
     cd $base
 done
 
-no_test_projects=(holo_gateway holo_control)
+no_test_projects=(holo_control holo_gateway)
 
 for project in ${no_test_projects[@]}
 do
