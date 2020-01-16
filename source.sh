db6ccde553c19166bea13683c78a960516547751
luopei <luopei@holomatic.com>
2020-01-16 19:47:50 +0800

    minor

--- source.sh
+++ source.sh
@@ -1,2 +1,5 @@
 export HOLO_ROOT=~/$1/output/native/relwithdebinfo/target/
 export LD_LIBRARY_PATH=${HOLO_ROOT}/3rdparty/lib:${HOLO_ROOT}/lib/:${LD_LIBRARY_PATH}
+
+export TENSORFLOW_BUILD_PATH=~/tensorflow
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/tensorflow/bazel-bin/tensorflow
