dc8aa57b27b3e74307c2830dff7c8dcbfb4758f5
luopei <luopei@holomatic.com>
2020-01-20 09:40:09 +0800

    fix tensorflow env

--- source.sh
+++ source.sh
@@ -1,5 +1,5 @@
 export HOLO_ROOT=~/$1/output/native/relwithdebinfo/target/
 export LD_LIBRARY_PATH=${HOLO_ROOT}/3rdparty/lib:${HOLO_ROOT}/lib/:${LD_LIBRARY_PATH}
 
-export TENSORFLOW_BUILD_PATH=~/tensorflow
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/tensorflow/bazel-bin/tensorflow
+export TENSORFLOW_BUILD_PATH=/opt/holo/tensorflow
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/holo/tensorflow/bazel-bin/tensorflow
