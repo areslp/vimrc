dfdf23924b2e721162a0a8a356c2e8500486349d
luopei <luopei@holomatic.com>
2022-10-24 22:28:38 +0800

    Add: nerdcommenter

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -78,6 +78,8 @@ return require('packer').startup(function(use)
 
     use {'neoclide/coc.nvim', branch = 'release'}
 
+    use {'preservim/nerdcommenter'}
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
